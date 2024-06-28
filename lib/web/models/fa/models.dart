3f0dbab461085cfdeafe06d56c2b11297058991e
JGM-edu <jmortiger@g.rit.edu>
2024-06-28 09:09:26 -0400

    Added fa post/submission model

--- lib/web/models/fa/models.dart
+++ lib/web/models/fa/models.dart
@@ -2,6 +2,9 @@
 //https://www.furaffinity.net/robots.txt
 //https://furaffinity-api.herokuapp.com/docs
 import 'package:fuzzy/util/util.dart';
+import 'package:fuzzy/web/models/image_listing.dart';
+import 'package:j_util/fa.dart' as fa;
+import 'package:j_util/j_util_full.dart';
 
 class Cookie {
   final String name;
@@ -38,4 +41,84 @@ class RequestBody {
         "cookies": cookies,
         "bbcode": bbcode,
       };
+}
+
+class Post extends fa.Post implements PostListingBare {
+  @override
+  final TagData tagData;
+  Post({
+    required super.id,
+    required super.title,
+    required super.author,
+    required super.date,
+    required super.tags,
+    required super.category,
+    required super.species,
+    required super.gender,
+    required super.rating,
+    required super.type,
+    required super.stats,
+    required super.description,
+    required super.footer,
+    required super.mentions,
+    required super.folder,
+    required super.userFolders,
+    required super.fileUrl,
+    required super.thumbnailUrl,
+    required super.comments,
+    required super.prev,
+    required super.next,
+    required super.favorite,
+    required super.favoriteToggleLink,
+  }) : tagData = TagData(general: tags, species: [species]),
+        preview = ImageInfoBare(url: thumbnailUrl),
+        file = ImageInfoBare(url: fileUrl);
+
+  @override
+  DateTime get createdAt => super.date;
+
+  @override
+  ImageInfoBare file;
+
+  @override
+  bool get isFavorited => favorite;
+
+  @override
+  final ImageInfoBare preview;
+
+  @override
+  List<String> get tagList => tagData.all;
+}
+
+class TagData implements ITagData {
+  @override
+  final List<String> general;
+
+  @override
+  final List<String> species;
+
+  List<String> get all => [...general, ...species];
+
+  TagData({required this.general, required this.species});
+}
+
+class ImageInfoBare implements IImageInfoBare {
+  final _address = LateFinal<Uri>();
+  @override
+  Uri get address =>
+      _address.isAssigned ? _address.$ : _address.itemSafe = Uri.parse(url);
+
+  @override
+  String get extension => url.substring(url.lastIndexOf(".") + 1);
+
+  @override
+  bool get hasValidUrl => IImageInfoBare.hasValidUrlImpl(this);
+
+  @override
+  bool get isAVideo => IImageInfoBare.isAVideoImpl(this);
+
+  @override
+  final String url;
+
+  ImageInfoBare({required this.url});
 }
