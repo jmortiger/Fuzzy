4d209cbfb4bb26ab67c0189ff5304fc4fc05f786
JGM-edu <jmortiger@g.rit.edu>
2024-06-28 09:08:47 -0400

    Refactor to reflect intersection of e6 and fa api responses

--- lib/web/models/image_listing.dart
+++ lib/web/models/image_listing.dart
@@ -1,18 +1,41 @@
-abstract interface class PostListing {
+abstract interface class PostListing extends PostListingBare {
+  @override
   IImageInfo get file;
+  @override
   IImageInfo get preview;
   ISampleInfo get sample;
-  int get id;
 }
 
-abstract interface class IImageInfo {
+abstract interface class PostListingBare {
+  int get id;
+  DateTime get createdAt;
+  List<String> get tagList;
+  ITagData get tagData;
+  IImageInfoBare get file;
+  IImageInfoBare get preview;
+  String get description;
+  bool get isFavorited;
+}
+
+abstract interface class ITagData {
+  List<String> get general;
+  List<String> get species;
+}
+
+abstract interface class IImageInfoBare {
   Uri get address;
   String get url;
+  bool get hasValidUrl => Uri.tryParse(url) != null;
+  static bool hasValidUrlImpl(IImageInfoBare i) => Uri.tryParse(i.url) != null;
+  String get extension;
+  static String extensionImpl(IImageInfoBare i) => i.url.substring(i.url.lastIndexOf(".") + 1);
+  bool get isAVideo => extension == "webm" || extension == "mp4";
+  static bool isAVideoImpl(IImageInfoBare i) => i.extension == "webm" || i.extension == "mp4";
+}
+
+abstract interface class IImageInfo extends IImageInfoBare {
   int get width;
   int get height;
-  bool get hasValidUrl => Uri.tryParse(url) != null;
-  String get extension;
-  bool get isAVideo => extension == "webm" || extension == "mp4";
 }
 
 abstract interface class ISampleInfo extends IImageInfo {
